f833929c04fa9fdb52e35dd5009ae247f3649569
Simranjit Singh <ssingh2937@conestogac.on.ca>
2021-02-15 17:35:46 -0500

    Completed rectangle class

---
+++ RectangleAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RectangleAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ RectangleAssignment/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace RectangleAssignment
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int l, int w)
+        {
+            length = l;
+            length = w;
+        }
+
+        public int GetLength() { return length; }
+
+        public int GetWidth() { return width; }
+
+        public int GetPerimeter() { return 2 * (length + width); }
+
+        public int GetArea() { return length * width; }
+
+        public int setLength(int len) { length = len; return length; }
+
+        public int setWidth(int wid) { width = wid; return width; }
+
+
+    }
+}
